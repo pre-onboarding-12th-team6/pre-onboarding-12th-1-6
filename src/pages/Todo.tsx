11209c55e9283bb6811ec2308c4dec6a726d1310
thumbthing <ilscc0902@gmail.com>
2023-08-24 15:34:28 +0900

    Test: TodoItem 내에서 사용되는 데이터, 기능 임시 선언

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import TodoItem from 'components/todo/TodoItem';
+import React, { useCallback, useEffect, useState } from 'react';
+
+export interface TodoProps {
+	id: number;
+	todo: string;
+	isCompleted: boolean;
+	userId: number;
+}
 
 function Todo() {
-	return <div>Todo</div>;
+	// test data
+	const [testData] = useState<TodoProps[]>([
+		{
+			id: 1,
+			todo: 'todo2',
+			isCompleted: false,
+			userId: 1,
+		},
+		{
+			id: 2,
+			todo: 'todo3',
+			isCompleted: false,
+			userId: 1,
+		},
+		{
+			id: 3,
+			todo: 'todo3',
+			isCompleted: false,
+			userId: 1,
+		},
+	]);
+
+	// 데이터 확인용
+	useEffect(() => {
+		console.log(testData);
+	}, []);
+
+	const handleIsCompleted = useCallback(() => {
+		console.log('handle isCompleted');
+	}, []);
+
+	const handleIsEditing = useCallback(() => {
+		console.log('handle isEditing');
+	}, []);
+
+	const handleDeleteTodo = useCallback(() => {
+		console.log('handle deleteTodo');
+	}, []);
+
+	return (
+		<div>
+			{testData.map((item) => (
+				<li key={item.id}>
+					<TodoItem
+						todo={item}
+						handleIsCompleted={handleIsCompleted}
+						handleIsEditing={handleIsEditing}
+						handleDeleteTodo={handleDeleteTodo}
+					/>
+				</li>
+			))}
+		</div>
+	);
 }
 
 export default Todo;
