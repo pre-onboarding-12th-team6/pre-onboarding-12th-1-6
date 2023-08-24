f0b76ec5fa777afc8a89ed3b068a758856e84c99
thumbthing <ilscc0902@gmail.com>
2023-08-24 19:40:55 +0900

    Feat: 임시 인증을 통해 api로 Create, Read, Update 기능 추가

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,57 +1,70 @@
+import { getTodos, updateTodo } from 'api/todoApi';
 import TodoCreateForm from 'components/todo/TodoCreateForm';
 import TodoItem from 'components/todo/TodoItem';
+import TodoUpdateForm from 'components/todo/TodoUpdateForm';
 import React, { useCallback, useEffect, useState } from 'react';
-
-export interface TodoProps {
-	id: number;
-	todo: string;
-	isCompleted: boolean;
-	userId: number;
-}
+import { Todo as TodoType } from 'types/type';
 
 function Todo() {
-	// test data
-	const [testData, setTestData] = useState<TodoProps[]>([
-		{
-			id: 1,
-			todo: 'todo2',
-			isCompleted: false,
-			userId: 1,
-		},
-		{
-			id: 2,
-			todo: 'todo3',
-			isCompleted: false,
-			userId: 1,
-		},
-		{
-			id: 3,
-			todo: 'todo3',
-			isCompleted: false,
-			userId: 1,
-		},
-	]);
+	const [todoList, setTodoList] = useState<TodoType[]>([]);
+	const [isModifying, setIsModifying] = useState<number>();
 
-	// 데이터 확인용
+	// 임시 인증 토큰
+	const token =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imhqa2xAaGprbC5oamtsIiwic3ViIjo5OTQ2LCJpYXQiOjE2OTI4MTY4MDMsImV4cCI6MTY5MzQyMTYwM30.OXEo2TovoPgLg9cApxL4A-lfnnB1PYdR2laNYtpZXiU';
+
+	// 서버로 부터 데이터 요청후 state에 set
 	useEffect(() => {
-		console.log(testData);
+		const getTodoList = async () => {
+			try {
+				const response = await getTodos();
+				setTodoList(response?.data);
+			} catch (error) {
+				alert(error);
+			}
+		};
+		// 임시 인증
+		const isTokenValid = localStorage.getItem('ACCESS_TOKEN');
+		if (!isTokenValid) {
+			localStorage.setItem('ACCESS_TOKEN', token);
+			getTodoList();
+		} else {
+			getTodoList();
+		}
 	}, []);
 
-	// set 매개변수 변경 필요 (현재는 test data로 진행)
 	const handleAddTodo = useCallback(
-		(newTodo: TodoProps) => {
-			setTestData([...testData, newTodo]);
+		(newTodo: TodoType) => {
+			setTodoList([...todoList, newTodo]);
 		},
-		[testData],
+		[todoList],
 	);
 
-	const handleIsCompleted = useCallback(() => {
-		console.log('handle isCompleted');
-	}, []);
+	const handleIsEditing = useCallback(
+		(todoId: number) => {
+			if (!isModifying) {
+				setIsModifying(todoId);
+			} else if (isModifying && todoId === isModifying) {
+				setIsModifying(undefined);
+			} else if (isModifying && todoId !== isModifying) {
+				setIsModifying(todoId);
+			}
+		},
+		[isModifying],
+	);
 
-	const handleIsEditing = useCallback(() => {
-		console.log('handle isEditing');
-	}, []);
+	const handleUpdateTodo = async (id: number, todo: TodoType) => {
+		try {
+			const response = await updateTodo(id, todo);
+			if (response.status === 200) {
+				const updatedTodo = todoList.map((item) => (item.id === response?.data.id ? { ...response?.data } : item));
+				setTodoList(updatedTodo);
+				setIsModifying(undefined);
+			}
+		} catch (error) {
+			alert(error);
+		}
+	};
 
 	const handleDeleteTodo = useCallback(() => {
 		console.log('handle deleteTodo');
@@ -61,14 +74,18 @@ function Todo() {
 		<div>
 			<TodoCreateForm handleAddTodo={handleAddTodo} />
 			<ul>
-				{testData.map((item) => (
+				{todoList.map((item) => (
 					<li key={item.id}>
-						<TodoItem
-							todo={item}
-							handleIsCompleted={handleIsCompleted}
-							handleIsEditing={handleIsEditing}
-							handleDeleteTodo={handleDeleteTodo}
-						/>
+						{isModifying !== item.id ? (
+							<TodoItem
+								todo={item}
+								handleUpdateTodo={handleUpdateTodo}
+								handleIsEditing={handleIsEditing}
+								handleDeleteTodo={handleDeleteTodo}
+							/>
+						) : (
+							<TodoUpdateForm todo={item} handleIsEditing={handleIsEditing} handleUpdateTodo={handleUpdateTodo} />
+						)}
 					</li>
 				))}
 			</ul>
