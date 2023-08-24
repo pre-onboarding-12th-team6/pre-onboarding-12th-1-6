048dc7c62ce4dd8dd83c69c1821f191d93cd99a4
thumbthing <ilscc0902@gmail.com>
2023-08-24 19:41:58 +0900

    Feat: Todo의 완료여부 Update 기능 추가

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { Todo } from 'types/type';
+import React, { useCallback } from 'react';
+import { Todo as TodoType } from 'types/type';
 
 interface TodoItemProps {
-	todo: Todo;
-	handleIsCompleted: () => void;
-	handleIsEditing: () => void;
+	todo: TodoType;
+	handleUpdateTodo: (id: number, todo: TodoType) => void;
+	handleIsEditing: (id: number) => void;
 	handleDeleteTodo: () => void;
 }
 
-function TodoItem({ todo, handleIsCompleted, handleIsEditing, handleDeleteTodo }: TodoItemProps) {
+function TodoItem({ todo, handleUpdateTodo, handleIsEditing, handleDeleteTodo }: TodoItemProps) {
+	const handleIsCompleted = useCallback(async () => {
+		try {
+			const newTodo: Partial<TodoType> = {
+				todo: todo.todo,
+				isCompleted: !todo.isCompleted,
+			};
+			handleUpdateTodo(todo.id, newTodo as TodoType);
+		} catch (error) {
+			alert(error);
+		}
+	}, []);
+
 	return (
 		<>
 			<label htmlFor="todo item">
-				<input type="checkbox" onChange={() => handleIsCompleted()} checked={todo.isCompleted} />
+				<input type="checkbox" onChange={handleIsCompleted} checked={todo.isCompleted} />
 				<span>{todo.todo}</span>
 			</label>
-			<button type="button" data-testid="modify-button" onClick={() => handleIsEditing()}>
+			<button type="button" data-testid="modify-button" onClick={() => handleIsEditing(todo.id)}>
 				수정
 			</button>
 			<button type="button" data-testid="delete-button" onClick={() => handleDeleteTodo()}>
