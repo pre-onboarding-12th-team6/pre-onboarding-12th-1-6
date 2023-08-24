a66e6a7c55ec43605fd434646016fcd28381f3de
337yj <337yje@gmail.com>
2023-08-25 00:27:29 +0900

    refactor : API함수 내 alert제거

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -2,41 +2,17 @@ import { Todo } from '../types/type';
 import apiClient from './apiClient';
 
 export const createTodo = (todo: Todo) => {
-	try {
-		return apiClient.post('/todos', todo);
-	} catch (error) {
-		const errorObj = error as Error;
-		alert(`Todo 생성 중 오류가 발생했습니다: ${errorObj.message}`);
-		throw errorObj;
-	}
+	return apiClient.post('/todos', todo);
 };
 
 export const getTodos = () => {
-	try {
-		return apiClient.get('/todos');
-	} catch (error) {
-		const errorObj = error as Error;
-		alert(`Todo 조회 중 오류가 발생했습니다: ${errorObj.message}`);
-		throw errorObj;
-	}
+	return apiClient.get('/todos');
 };
 
 export const updateTodo = (id: number, todo: Todo) => {
-	try {
-		return apiClient.put(`/todos/${id}`, todo);
-	} catch (error) {
-		const errorObj = error as Error;
-		alert(`Todo 업데이트 중 오류가 발생했습니다: ${errorObj.message}`);
-		throw errorObj;
-	}
+	return apiClient.put(`/todos/${id}`, todo);
 };
 
 export const deleteTodo = (id: number) => {
-	try {
-		return apiClient.delete(`/todos/${id}`);
-	} catch (error) {
-		const errorObj = error as Error;
-		alert(`Todo 삭제 중 오류가 발생했습니다: ${errorObj.message}`);
-		throw errorObj;
-	}
+	return apiClient.delete(`/todos/${id}`);
 };
